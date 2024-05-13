a7f4b20b8fe144a083103d659c36f9f4a7078cf6
Vitor Hugo <ovhpgs@gmail.com>
2024-05-13 14:44:00 -0300

    creation of the three initial classes

--- BasedRPG/PlayerClass.cs
+++ BasedRPG/PlayerClass.cs
@@ -2,9 +2,46 @@
 {
     class PlayerClass
     {
-        //each attribute depends on the class
         public int InitialHP;
         public int InitialSTR;
         public int InitialINT;
+
+
+        public class Warrior
+        {
+            private PlayerClass WarriorClass;
+
+            public Warrior()
+            {
+                WarriorClass.InitialHP = 100;
+                WarriorClass.InitialSTR = 14;
+                WarriorClass.InitialINT = 9;
+            }
+        }
+
+        public class Sorcerer
+        {
+            private PlayerClass SorcererClass;
+
+            public Sorcerer()
+            {
+                SorcererClass.InitialHP = 100;
+                SorcererClass.InitialSTR = 9;
+                SorcererClass.InitialINT = 15;
+            }
+        }
+
+        public class Thief
+        {
+            private PlayerClass ThiefClass;
+
+            public Thief()
+            {
+                ThiefClass.InitialHP = 100;
+                ThiefClass.InitialSTR = 9;
+                ThiefClass.InitialINT = 12;
+            }
+        }
+
     }
 }
